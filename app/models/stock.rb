1a037c4e4517915b072567fd5c2435cf34fd9dbf
sudarshan gupta <>
2021-04-06 17:19:36 +0000

    build class method for stock lookup

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,11 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+  client = IEX::Api::Client.new(
+    publishable_token: 'Tpk_d3773177900644bc81aa5c4a962d0dad',
+    secret_token: 'secret_token',
+    endpoint: 'https://sandbox.iexapis.com/v1'
+)
+  client.price(ticker_symbol)
+  end
 end
