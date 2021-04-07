afa7b38ab178b5177c5bef5e54ad586380bed3b8
sudarshan gupta <>
2021-04-07 07:19:45 +0000

    Dealing with invalid search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,11 @@ class Stock < ApplicationRecord
     publishable_token: 'Tpk_d3773177900644bc81aa5c4a962d0dad',
     secret_token: 'secret_token',
     endpoint: 'https://sandbox.iexapis.com/v1')
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
